1fe9d576d7faea23cefe05820f0fc9bb4c40d625
fhsk19 <36655646+fhsk19@users.noreply.github.com>
2018-02-21 12:53:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,13 @@ app.get('/counter',function(req,res){
 app.get('/article-three',function(req,res)  {
     res.send('Article three will be served here');
 });
-
+app.get('/submit-name/:name', function (req, res) {
+    //Get the name from the request
+    var name=req.params.name;
+    names.push(name);
+    //JSON: JavaScript Object Notation 
+    res.send(JSON.stringify(names)); //1000
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -32,13 +38,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/submit-name/:name', function (req, res) {
-    //Get the name from the request
-    var name=req.params.name;
-    names.push(name);
-    //JSON: JavaScript Object Notation 
-    res.send(JSON.stringify(names)); //1000
-});
+
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
