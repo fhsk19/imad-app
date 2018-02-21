df2d7d57904efda5c51b895242482d0a272a6998
fhsk19 <36655646+fhsk19@users.noreply.github.com>
2018-02-21 12:08:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,22 @@
 //Counter code
 var button= document.getElementById('counter');
-var counter=0;
 button.onclick=function (){
-    //Make a request to counter endpoint
+    //Creating a request onject
+    var request= new XMLHttpRequest();
     
     //Capture a response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readystate===XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status===200){
+                var counter=reqest.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }//Not done yet
+    }
+    //Make the request
+    request.open('GET','http://fhsk19.imad.hasura-app.io/counter',true);
+    request.send(null);
     
-    //Render the variable in correct span
-    counter= counter+1;
-    var span= document.getElementById('count');
-    span.innerHTML=counter.toString();
 };
