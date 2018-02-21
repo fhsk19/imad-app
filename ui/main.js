ab94bbe7c69533c0f1d5c3c4eca11f0459abbcc2
fhsk19 <36655646+fhsk19@users.noreply.github.com>
2018-02-21 11:56:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
-console.log('Loaded!');
-//Move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight (){
+//Counter code
+var button= document.getElementById('counter');
+var counter=0;
+button.onclick=function (){
+    //Make a request to counter endpoint
     
-    marginLeft= marginLeft+10;
-    img.style.marginLeft= marginLeft+'px';
-}
-img.onclick=function (){
-    var interval= setInterval(moveRight,100);
+    //Capture a response and store it in a variable
+    
+    //Render the variable in correct span
+    counter= counter+1;
+    var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
