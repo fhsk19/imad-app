a6730ef97b89cfb1390db6147793190fd4c4acc5
fhsk19 <36655646+fhsk19@users.noreply.github.com>
2018-02-21 13:02:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,31 +11,19 @@ button.onclick=function (){
         if(request.readystate===XMLHttpRequest.DONE){
             //Take some action
             if (request.status===200){
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-               
-      }
-        }//Not done yet
-    }
-    //Make the request
-    request.open('GET','http://fhsk19.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
-
-//Submit name
-var nameInput= document.getElementById('name');
-var name=nameInput.value;
-var submit= document.getElementById('submit_btn');
-submit.onclick=function(){
-    //Make a request to the server and send the name
-    //Capture a list and render it as a list
-    var names= ['name1','name2','name3','name4'];
+                //Capture a list and render it as a list
+    var names= request.responseText;
+    names=JSON.parse(names);
     var list='';
     for (var i=0; i<names.lenght; i++){
         list+='<li>'+ names[i] + '</li>';
     }
     var ul= document.getElementById('namelist');
     ul.innerHTML= list;
-    };
+      }
+        }//Not done yet
+};
+//Make a request
+request.open('GET', 'http://fhsk19.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
+};
